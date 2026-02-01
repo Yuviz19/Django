b049cd38b0757e4f4e07751c808202bbe21c4bc8
ByteVoid <es.yuviz19@gmail.com>
2026-02-01 14:47:11 +0530

    start byteTalks project

---
+++ byteTalks/byteTalks/base/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('hello')
+
+def room(request):
+    return HttpResponse('You are in the Backrooms')

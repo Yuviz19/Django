ba9aab9626ed8aebbb6fb96d0398f61cd79a873f
ByteVoid <es.yuviz19@gmail.com>
2026-02-12 16:10:09 +0530

    add project databases/models & basic admin confs

--- byteTalks/byteTalks/base/views.py
+++ byteTalks/byteTalks/base/views.py
@@ -1,18 +1,16 @@
 from django.shortcuts import render
-
-rooms = [
-    {"id": 1, "name": "Learn Python Together"},
-    {"id": 2, "name": "Designn With Me"},
-    {"id": 3, "name": "Frontend Developers"}
-]
+from .models import Room
+# rooms = [
+#     {"id": 1, "name": "Learn Python Together"},
+#     {"id": 2, "name": "Designn With Me"},
+#     {"id": 3, "name": "Frontend Developers"}
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     return render(request, 'base/home.html',{"rooms":rooms})
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {"rooms": room}
     return render(request, 'base/room.html', context)
